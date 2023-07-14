1280131b83fc4f4059ba58da65d0f71bbf009a81
Mimifresh <mimigolld@gmail.com>
2023-07-13 17:02:31 -0700

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+5-print_comb2.py
+5-print_comb2.py
+    print("{}".format(sum))
