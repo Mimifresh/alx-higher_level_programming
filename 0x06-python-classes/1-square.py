ba8eccc76ce724bcca186e51c7af6578e13c2c7e
Mimifresh <mimigolld@gmail.com>
2023-08-01 19:26:52 -0700

    private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class of square that defines a square"""
+
+
+class Square:
+    """just pass"""
+
+    def __init__(self, size):
+        """private instance atribut"""
+        self.size_sq = size
