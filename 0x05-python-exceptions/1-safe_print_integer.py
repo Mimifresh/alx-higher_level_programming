eaa0a1fabfac2fa3f545b8efe1646dc4e047f4e4
Mimifresh <mimigolld@gmail.com>
2023-08-03 15:39:23 -0700

    A function to print integers

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pyhon3
+def safe_print_integer(value):
+    try:
+        int(value)
+        print(value)
+
+        return True
+
+    except (TypeError, ValueError):
+
+        return False
+
+            
